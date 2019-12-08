408035061ee4f9298bec553d97190b0611d65209
Larry-nan <lawrence,yinan@gmail.com>
2019-12-07 16:24:41 -0800

    add more function and readme

--- index.js
+++ index.js
@@ -15,6 +15,81 @@ function newGame() {
     correctWord = new Word(randomWord)
     guessLeft = correctWord.array.length
 }
-displayLetter = correctWord.keywordString()
-console.log(displayLetter);
-correctWord.compare = displayLetter
+
+function startGame() {
+    inquirer.prompt([{
+        name: "ask",
+        message: "Guess a letter"
+    }]).then(function (response) {
+        var input = response.ask
+        if (lives > 0) {
+            if (input.length === 1) {
+                correctWord.guessCheck(input)
+                displayLetter = correctWord.keywordString()
+                if (correctWord.compare === displayLetter) {
+                    console.log("Sorry", input, "not in word")
+                    lives--
+                    console.log("Only", lives, "guesse(s) left.")
+                    if (lives === 0) {
+                        gameOver()
+                    } else {
+                        print()
+                        startGame()
+                    }
+                } else {
+                    console.log("Well done!")
+                    guessLeft--
+                    print();
+                    if (guessLeft === 0) {
+                        console.log("You are right, keep guessing:");
+                        newGame()
+                        print();
+                        startGame();
+                    } else {
+                        startGame()
+                    }
+                }
+            } else if (input.length === 0) {
+                consoel.log("Choose something.");
+                startGame()
+            } else {
+                console.log("Only one letter.")
+                startGame()
+            }
+        } else {
+            gameOver()
+        }
+    })
+}
+
+function displayWord() {
+    displayLetter = correctWord.keywordString()
+    console.log(displayLetter);
+    correctWord.compare = displayLetter
+}
+
+function gameOver() {
+    {
+        console.log("You loss")
+        inquirer.prompt([{
+            type: "confirm",
+            name: "playAgain",
+            message: "Try again?"
+        }]).then(function (response) {
+            if (response.playAgain) {
+                newGame()
+                print()
+                startGame();
+            } else {
+                console.log("bye bye")
+            }
+        })
+    }
+}
+
+function print() {
+    displayWord()
+}
+newGame()
+print()
+startGame();
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Constructor Word Guess Game
+----
+## Instructions:
+
+- On start, you will be given a random word
+- Enter letters one at a time to start guessing the word
+- If you guess a correct letter, it will fill in the corresponding blank(s)
+- If you guess an incorrect letter, your "guesses remaining" will decrease
+
+## If you correctly guess the word, "You got it right!" will display
+
+## If you run out of guesses, "Out of guesses!" will display
+
+## If you guess the word correctly or run out of guesses, you will be asked if you want a new word
+## If you answer "Yes", you will be given a new random word
+## If you answer "No", the game will stop
+
+# Technologies Used:
+
+- JavaScript, Node.js, Command Line, Inquirer, Constructors
